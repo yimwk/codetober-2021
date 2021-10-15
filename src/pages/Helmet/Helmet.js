b8702868a7c3b8e6e425c0d6f2e75973f6e99ef2
Yim Wai Kei <yimwaikei@gmail.com>
2021-10-15 16:51:05 +0800

    feat: day 15 - helmet
    added red hood, since squid game's guards are wearing mask, not helmet

--- src/pages/Helmet/Helmet.js
+++ src/pages/Helmet/Helmet.js
@@ -6,19 +6,60 @@ class Helmet extends React.Component {
     constructor(props) {
         super(props);
 
-        this.helmets = ["helmet-square", "helmet-triangle", "helmet-circle"];
+        this.helmets = ["squid-game-helmet-square", "squid-game-helmet-triangle", "squid-game-helmet-circle"];
+
+        this.state = {
+            showBatSymbol: true,
+            showRedHoodHelmet: false
+        };
+
+        setTimeout(() => this.setState({
+            ...this.state,
+            showBatSymbol: false,
+            showRedHoodHelmet: true
+        }), 10000);
     }
 
     render() {
         return <><h1>Day 15: Helmet</h1>
-            <div className="squid-game">
-                {this.helmets.map(helmet => {
-                    return <div className="helmet">
-                        <div className="helmet-horizontal-bar"></div>
-                        <div className="helmet-vertical-bar"></div>
-                        <div className={helmet}></div>
+            <div className="red-hood">
+                {this.state.showBatSymbol && (
+                    <div className="bat-symbol">
+                        <div className="batman-symbol-wing batman-symbol-wing-left"></div>
+                        <div className="batman-symbol-wing batman-symbol-wing-right"></div>
+                        <div className="batman-symbol-tail"></div>
+                        <div className="batman-symbol-thorn batman-symbol-thorn-left-end"></div>
+                        <div className="batman-symbol-thorn batman-symbol-thorn-left-middle"></div>
+                        <div className="batman-symbol-thorn batman-symbol-thorn-right-end"></div>
+                        <div className="batman-symbol-thorn batman-symbol-thorn-right-middle"></div>
+                        <div className="batman-symbol-head"></div>
+                        <div className="batman-symbol-ear batman-symbol-left-ear"></div>
+                        <div className="batman-symbol-ear batman-symbol-right-ear"></div>
                     </div>
-                })}
+                )}
+                {this.state.showRedHoodHelmet && (
+                    <div className="red-hood-helmet">
+                        <div className="red-hood-helmet-top-vertical-bar red-hood-helmet-top-vertical-bar-left"></div>
+                        <div className="red-hood-helmet-top-vertical-bar red-hood-helmet-top-vertical-bar-right"></div>
+                        <div className="red-hood-eye red-hood-eye-left"></div>
+                        <div className="red-hood-eye red-hood-eye-right"></div>
+                        <div className="red-hood-helmet-bottom-vertical-bar red-hood-helmet-bottom-vertical-bar-left"></div>
+                        <div className="red-hood-helmet-bottom-vertical-bar red-hood-helmet-bottom-vertical-bar-right"></div>
+                        <div className="red-hood-helmet-top-horizontal-bar"></div>
+                    </div>
+                )}
+            </div>
+            <div>
+                Previous Attempt:
+                <div className="squid-game">
+                    {this.helmets.map(helmet => {
+                        return <div className="squid-game-helmet">
+                            <div className="squid-game-helmet-horizontal-bar"></div>
+                            <div className="squid-game-helmet-vertical-bar"></div>
+                            <div className={helmet}></div>
+                        </div>
+                    })}
+                </div>
             </div>
         </>
     }
